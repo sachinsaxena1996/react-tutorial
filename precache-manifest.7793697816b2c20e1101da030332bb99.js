b53829b7b813a62e24c87235c8c04911d290ee19
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2019-03-06 13:27:09 +0530

    Updates

---
+++ precache-manifest.7793697816b2c20e1101da030332bb99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "bedc1dc5783b6109c696",
+    "url": "/react-tutorial/static/js/main.bedc1dc5.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/react-tutorial/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "bedc1dc5783b6109c696",
+    "url": "/react-tutorial/static/css/main.54b4e833.chunk.css"
+  },
+  {
+    "revision": "7286ec2bc80cec58e44c3fcb8560cb11",
+    "url": "/react-tutorial/index.html"
+  }
+];
